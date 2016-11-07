17fc2e06538ec57cd20a1196868b12b1ceca27d9
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-11-07 15:32:19 +0100

    add unit test for Shared Project HAppi and travis file

---
+++ HAppi.ClassLibrary.UnitTests/DummyTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace HAppi.ClassLibrary.UnitTests
+{
+    public class DummyTests
+    {
+        [Test]
+        public static void TestTrue()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public static void TestTestMe()
+        {
+            Assert.AreEqual(1, MyClass.TestMe());
+        }
+    }
+}
