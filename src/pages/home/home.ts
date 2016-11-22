fcd40164b6d8e147f1c08dbd2e71fdc4751fc5a9
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-11-22 14:39:14 +0100

    basic symptoms list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -8,8 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  symptoms : any;
 
+  constructor(public navCtrl: NavController) {
+    this.symptoms = [
+      'Headache',
+      'Fever',
+      'Diarrhea'
+    ];
   }
 
 }
