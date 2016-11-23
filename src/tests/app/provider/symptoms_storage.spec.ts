b5f3066c6ec3541fc26b21cd57a1babf40eb1f58
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-11-23 16:00:50 +0100

    add dummy unit test following tuto: http://lathonez.github.io/2016/ionic-2-unit-testing/

---
+++ src/tests/app/provider/symptoms_storage.spec.ts
@@ -0,0 +1,13 @@
+import {} from 'jasmine';
+import { ComponentFixture, async } from '@angular/core/testing';
+import { TestUtils }               from '../../../test';
+import { SymptomsStorage }          from '../../../app/provider/symptoms_storage';
+
+let fixture: ComponentFixture<SymptomsStorage> = null;
+let instance: any = null;
+
+describe('Pages: HelloIonic', () => {
+  it('should create the hello ionic page', async(() => {
+    expect(true).toBeTruthy();
+  }));
+});
