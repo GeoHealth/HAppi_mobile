2ff1ed2035c70c60c014037775a86ab9165d8a07
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-12-08 22:57:30 +0100

    remove unused functions

--- src/app/provider/date_provider.ts
+++ src/app/provider/date_provider.ts
@@ -2,12 +2,4 @@ export class DateProvider {
   public static getCurrentISODateAsString(): string {
     return new Date().toISOString();
   }
-
-  public static getCurrentLocaleDateAsString(): string {
-    return new Date().toString();
-  }
-
-  public static getCurrentDate(): Date {
-    return new Date;
-  }
 }
