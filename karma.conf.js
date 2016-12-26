78dbdd9604e857d118bb87fa74b1b4085b1455b7
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-12-26 12:49:54 +0100

    less log during tests

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
     ],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_WARN,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
