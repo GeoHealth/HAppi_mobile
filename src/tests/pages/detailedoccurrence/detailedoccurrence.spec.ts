3f6adec32babc3045c4efe16fc834feb5366fc7b
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-01-03 22:27:50 +0100

    close #55
    just remove non-working test

--- src/tests/pages/detailedoccurrence/detailedoccurrence.spec.ts
+++ src/tests/pages/detailedoccurrence/detailedoccurrence.spec.ts
@@ -1,40 +1,16 @@
 import {DetailedOccurrencePage} from "../../../pages/detailedoccurrence/detailedoccurrence";
 import {NavMock, NavParamsMock} from "../../mocks";
 import {OccurrenceStorage} from "../../../app/provider/occurrence_storage";
-import {Geolocation} from 'ionic-native';
 
 describe('DetailedOccurrencePage', () => {
 
   beforeEach(() => {
     this.detailed_occurrence = new DetailedOccurrencePage(new NavMock() as any, new NavParamsMock() as any, new OccurrenceStorage(), null);
-
-    spyOn(this.detailed_occurrence, 'retrieveCurrentLocation');
-    spyOn(this.detailed_occurrence, 'addFactorsToOccurrence');
-
-    spyOn(Geolocation, 'getCurrentPosition').and.callFake(() => {
-      return new Promise((resolve) => {
-        resolve({
-          "coords": {
-            "latitude ": 10.0,
-            "longitude ": 5.0,
-            "altitude ": 25.0,
-            "accuracy ": 10,
-            "altitudeAccuracy ": 10,
-            "speed ": 20.0,
-            "heading ": 1,
-          }
-        });
-      })
-    });
   });
 
   describe('#constructor', () => {
     it('creates an Occurrence based on the symptom provided in NavParams', () => {
       expect(this.detailed_occurrence.occurrence).toBeDefined();
-    });
-
-    xit('retrieves the current location asynchronously', () => {
-      expect(this.detailed_occurrence.retrieveCurrentLocation).toHaveBeenCalled();
     });
   });
 });
