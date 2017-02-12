ce621fcc9c43904379d8dd9c0afa7c1ccf62401c
Kagelmacher <alexandre.hauet@gmail.com>
2017-02-12 21:28:00 +0100

    login

--- src/app/provider/auth_storage.ts
+++ src/app/provider/auth_storage.ts
@@ -8,40 +8,43 @@ let localForage = require('localforage');
 @Injectable()
 export class AuthStorage {
 
-  private inMemoryDB: any;
-  store: any;
-  private headers: Headers;
+    private inMemoryDB: any;
+    store: any;
+    private headers: any;
 
-  constructor() {
-    this.initStore();
-    this.initInMemoryDB();
-  }
-
-  private initStore() {
-    this.store = localForage.createInstance({
-      name: 'auth happi'
-    });
-    this.store.setDriver([localForage.INDEXEDDB, localForage.WEBSQL, localForage.LOCALSTORAGE]);
-  }
-
-  private initInMemoryDB() {
-    this.inMemoryDB = new loki('headers');
-    this.headers = this.inMemoryDB.addCollection('headers');
-  };
-
-  get(): Headers {
-    return this.headers;
-  }
-
-  save(headers: Headers) {
-      console.log(headers);
-    if (headers instanceof Headers) {
-      this.store.setItem('headers', JSON.stringify(this.inMemoryDB)).then((value) => {
-      }).catch((err) => {
-
-      });
-    } else {
-      throw new TypeError("Wrong type adding to header storage");
+    constructor() {
+        this.initStore();
+        this.initInMemoryDB();
     }
-  };
+
+    private initStore() {
+        this.store = localForage.createInstance({
+            name: 'auth happi'
+        });
+        this.store.setDriver([localForage.INDEXEDDB, localForage.WEBSQL, localForage.LOCALSTORAGE]);
+    }
+
+    private initInMemoryDB() {
+        this.inMemoryDB = new loki('headers');
+        this.headers = this.inMemoryDB.addCollection('headers');
+    };
+
+
+    get(): Headers {
+        return this.headers;
+    }
+
+    save(headers: Headers) {
+        console.log(headers);
+        if (headers instanceof Headers) {
+            this.headers.insert(headers);
+            this.store.setItem('headers', JSON.stringify(this.inMemoryDB)).then((value) => {
+
+            }).catch((err) => {
+
+            });
+        } else {
+            throw new TypeError("Wrong type adding to header storage");
+        }
+    };
 }
