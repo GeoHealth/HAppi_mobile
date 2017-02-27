d6311c67095882b67cd8c13f43d87f1231d61fd3
Kagelmacher <alexandre.hauet@gmail.com>
2017-02-27 15:23:18 +0100

    close #61 : implementation of the chart'

--- src/pages/statistic/statistic.ts
+++ src/pages/statistic/statistic.ts
@@ -5,6 +5,8 @@ import {StatsRestService} from '../../app/services/stats_rest_service';
 import {SymptomsCounts} from '../../models/symptoms_counts';
 import {GlobalVars} from '../../app/provider/global_vars';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'page-statistic',
   templateUrl: 'statistic.html'
@@ -15,39 +17,64 @@ export class StatisticPage {
 
   dailyChart: any;
 
-  private colors: string[] = ["rgba(255,0,0,1)", "rgba(0,0,255,1)"]
+  //private colors: string[] = ["rgba(255,0,0,1)", "rgba(0,0,255,1)"]
 
 
   private symptoms_counts: SymptomsCounts;
 
+  private start_date;
+  private end_date;
 
   constructor(public translation: TranslationProvider, private stats_rest_service: StatsRestService, public vars: GlobalVars) {
     this.vars.setTitle("Statistics");
   }
 
-  ionViewDidLoad() {
+  ionViewDidEnter() {
+    this.end_date = moment(new Date(moment().get('year'), moment().get('month'), moment().get('date'), 23, 59, 59));
+    this.start_date = moment(this.end_date).subtract(7, 'days');
+    this.draw("days");
 
-    this.stats_rest_service.getAverage().subscribe((success) => {
+  }
+
+  onPageWillEnter() {
+    this.draw("days");
+  }
+
+  draw(unit: string) {
+    this.stats_rest_service.getCount(moment(this.start_date).format(), moment(this.end_date).format(), unit).subscribe((success) => {
       if (success) {
         this.symptoms_counts = success;
 
-        let labels = ["Lundi", "Mardi", "Mercrdi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
         let datasets = [];
+
+        let labels = [];
+
+        if (this.symptoms_counts.symptoms.length > 0) {
+          let symptom = this.symptoms_counts.symptoms[0];
+
+          for(let x = 0; x < symptom.counts.length; x++) {
+            labels.push(moment(symptom.counts[x].date).format('MM-DD-YYYY'));
+          }
+
+        }
 
         for(let line = 0; line < this.symptoms_counts.symptoms.length; line++) {
           let y = [];
           let symptom = this.symptoms_counts.symptoms[line];
           let data = [];
-          console.log(symptom);
+
           for(let x = 0; x < symptom.counts.length; x++) {
             data.push(symptom.counts[x].count)
           }
+
+          let color = this.getRandomColor()
+
           let dataset = {
             label: symptom.name,
             fill: false,
             lineTension: 0.1,
-            borderColor: this.colors[line],
             borderCapStyle: 'butt',
+            borderColor: color,
             borderDash: [],
             borderDashOffset: 0.0,
             borderJoinStyle: 'miter',
@@ -67,13 +94,58 @@ export class StatisticPage {
           data: {
             labels: labels,
             datasets: datasets
+          },
+          options: {
+            responsive: true,
+            scales: {
+              yAxes: [{
+                ticks: {
+                  suggestedMax: 10,
+                  min: 0,
+                  stepSize: 1
+                }
+              }]
+            },
+            legend: {
+              position: 'bottom',
+              labels : {
+                usePointStyle: true
+              }
+            }
           }
         });
       }
-    })
+    });
+  }
 
+  public drawPrevious() {
+    this.start_date = moment(this.start_date).subtract(7, 'days');
+    this.end_date = moment(this.end_date).subtract(7, 'days');
+    this.draw("days");
+  }
+
+  public drawNext() {
+    this.start_date = moment(this.start_date).add(7, 'days');
+    this.end_date = moment(this.end_date).add(7, 'days');
+    this.draw("days");
   }
 
 
+  public getStartDate() {
+    return moment(this.start_date).format('DD/MM/YYYY');
+  }
 
+  public getEndDate() {
+    return moment(this.end_date).format(' DD/MM/YYYY');
+  }
+
+
+  getRandomColor() {
+    let letters = '0123456789ABCDEF'.split('');
+    let color = '#';
+    for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+  }
 }
