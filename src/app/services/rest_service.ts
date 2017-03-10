01e3a8c70dc086dcc6bcd77dcaa9733d465a1dbb
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-03-10 17:50:42 +0100

    fail safe version of HandleError in rest_service because if the response has no body, it cannot be parsed to json and raise an error

--- src/app/services/rest_service.ts
+++ src/app/services/rest_service.ts
@@ -63,16 +63,20 @@ export class RestService {
   }
 
   static handleError(error: Response | any) {
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
+    try {
+      let errMsg: string;
+      if (error instanceof Response) {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+      } else {
+        errMsg = error.message ? error.message : error.toString();
+      }
+      (<any>window).fabric.sendNonFatalCrashWithStacktraceCreation(errMsg);
+      return Observable.throw(errMsg);
+    } catch (e) {
+      return Observable.throw(error);
     }
-    (<any>window).fabric.sendNonFatalCrashWithStacktraceCreation(errMsg);
-    return Observable.throw(errMsg);
   }
 
   static handleErrorWithoutParsing(error: Response | any) {
