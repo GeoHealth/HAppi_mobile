57b8a76a5b0c57ed23b219f88c6152cd0d326dba
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-03-20 16:32:24 +0100

    ref #94
    SHow error message in delete occurrence too

--- src/pages/occurrence/occurrence.ts
+++ src/pages/occurrence/occurrence.ts
@@ -4,6 +4,8 @@ import {TranslationProvider} from "../../app/provider/translation_provider";
 import {GlobalVars} from "../../app/provider/global_vars";
 import {Occurrence} from "../../models/occurrence";
 import {OccurrenceRestService} from "../../app/services/occurrence_rest_service";
+import {ToastController} from "ionic-angular";
+import {Crashlytics} from "../../app/services/crashlytics";
 
 @Component({
   selector: 'page-occurrence',
@@ -13,7 +15,9 @@ export class OccurrencePage {
 
   occurrences_storage: OccurrenceStorage;
 
-  constructor(occurrence_storage: OccurrenceStorage, public translation: TranslationProvider, public vars: GlobalVars, private occurrences_rest_service: OccurrenceRestService) {
+  constructor(occurrence_storage: OccurrenceStorage, public translation: TranslationProvider, public vars: GlobalVars,
+              private occurrences_rest_service: OccurrenceRestService, private toastCtrl: ToastController,
+              private crashlytics: Crashlytics) {
     this.occurrences_storage = occurrence_storage;
   }
 
@@ -21,10 +25,21 @@ export class OccurrencePage {
     this.vars.setTitle("Occurence");
   }
 
-  deleteOccurrence(occurrence: Occurrence) {
+  deleteOccurrence(occurrence: Occurrence) {
     this.occurrences_rest_service.deleteOccurrence(occurrence).subscribe((result) => {
       this.occurrences_storage.remove(occurrence);
+    }, (err) => {
+      this.presentToastError();
+      this.crashlytics.sendNonFatalCrashWithStacktraceCreation(err.message);
     });
   }
 
+  private presentToastError() {
+    let toast = this.toastCtrl.create({
+      message: 'Error while deleting the occurrence',
+      duration: 10000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
 }
