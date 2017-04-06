5f1194f52839b20cebfeabcd82ab4206fae406aa
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-04-06 17:16:55 +0200

    fix: rematch the signature

--- src/tests/mocks.ts
+++ src/tests/mocks.ts
@@ -128,6 +128,6 @@ export class OccurrenceStorageMock extends OccurrenceStorage {
 
 export class ToastControllerMock extends ToastController {
   constructor(){
-    super(null, null);
+    super(null);
   }
 }
