5e0419800a410eb1a6d7b58cd083cfdbd0ef4a8f
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-04-11 21:38:56 +0200

    ref #133 Use Translation + change conditions to english

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,8 +1,9 @@
-import {Component} from "@angular/core";
-import {NavController, AlertController} from "ionic-angular";
-import {AuthService} from "../../app/provider/auth_service";
-import {TabsPage} from "../tabs/tabs";
-import {Crashlytics} from "../../app/services/crashlytics";
+import { Component } from "@angular/core";
+import { NavController, AlertController } from "ionic-angular";
+import { AuthService } from "../../app/provider/auth_service";
+import { TabsPage } from "../tabs/tabs";
+import { Crashlytics } from "../../app/services/crashlytics";
+import { TranslationProvider } from "../../app/provider/translation_provider";
 
 @Component({
   selector: 'page-register',
@@ -12,7 +13,8 @@ export class RegisterPage {
   createSuccess = false;
   registerCredentials = {email: '', password: '', condition: false};
 
-  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController, private crashlytics: Crashlytics) {
+  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController,
+              private crashlytics: Crashlytics, public translation: TranslationProvider) {
   }
 
   public register() {
@@ -20,21 +22,21 @@ export class RegisterPage {
       this.auth.register(this.registerCredentials).subscribe((res) => {
         if (res.success) {
           this.createSuccess = true;
-          this.showPopup("Success", "Account created.");
+          this.showPopup(this.translation.gettext("Success"), this.translation.gettext("Account created."));
         } else {
-          this.showPopup("Error", res.msg);
+          this.showPopup(this.translation.gettext("Error"), res.msg);
         }
       }, (err) => {
         this.crashlytics.sendNonFatalCrashWithStacktraceCreation(err);
-        this.showPopup("Error", err);
+        this.showPopup(this.translation.gettext("Error"), err);
       });
     } else {
-      this.showPopup("Error", "You must accept the conditions to use the application !");
+      this.showPopup(this.translation.gettext("Error"), this.translation.gettext("You must accept the conditions to use the application !"));
     }
   }
 
   showCondition() {
-    this.showPopup("Conditions", "J'accepte que mes données personnelles soient enregistrer et utilisées");
+    this.showPopup(this.translation.gettext("Conditions"), this.translation.gettext("By registering to this product, I agree that my personal information will be stored and used for an undefined time. I am aware that this service is not yet finished and my data might be lost at any time, without any warning."));
   }
 
   showPopup(title, text) {
