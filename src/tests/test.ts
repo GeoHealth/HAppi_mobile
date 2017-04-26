bdf2db31ae404c99a08dfaef12057c958cfb17eb
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-04-26 09:58:28 +0200

    ref #142 lint test

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -7,11 +7,6 @@ import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TestBed} from '@angular/core/testing';
-import {App, MenuController, NavController, Platform, Config, Keyboard, Form, IonicModule}  from 'ionic-angular';
-import {ConfigMock} from './mocks';
-
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
 declare var require: any;
