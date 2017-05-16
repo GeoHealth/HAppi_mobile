f5fb8e0c2349cc153b15a82abb26a11ffece64d1
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-05-16 11:07:31 +0200

    ref #143 unit test Crashlytics

--- src/tests/app/services/crashlytics.spec.ts
+++ src/tests/app/services/crashlytics.spec.ts
@@ -5,303 +5,327 @@ import * as stacktrace from "stacktrace-js";
 
 
 describe('Crashlytics', () => {
-  beforeAll(() => {
-    this.crashlytics = new Crashlytics(new PlatformMock() as Platform);
-  });
 
-  afterAll(() => {
-    this.crashlytics = null;
-  });
-
-  describe('#sendNonFatalCrashWithStacktraceCreation', () => {
-    it('calls sendNonFatalCrash', (done) => {
-      spyOn(this.crashlytics, 'sendNonFatalCrash').and.stub();
-      spyOn(stacktrace, 'get').and.returnValue(new Promise((resolve, reject) => {
-        resolve(null);
-      }));
-      this.crashlytics.sendNonFatalCrashWithStacktraceCreation();
-      setTimeout(
-        () => {
-          expect(this.crashlytics.sendNonFatalCrash).toHaveBeenCalled();
-          done();
-        },
-        100);
-    });
-  });
-
-  describe('when no handler is set', () => {
+  describe('with a window.fabric instance', () => {
     beforeEach(() => {
-      this.crashlytics['handler'] = undefined;
-      spyOn(console, 'warn').and.stub();
-    });
-
-    describe('#setUserIdentifier', () => {
-      beforeEach(() => {
-        this.crashlytics.setUserIdentifier(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#setUserName', () => {
-      beforeEach(() => {
-        this.crashlytics.setUserName(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#setUserEmail', () => {
-      beforeEach(() => {
-        this.crashlytics.setUserEmail(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#addLog', () => {
-      beforeEach(() => {
-        this.crashlytics.addLog(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendNonFatalCrash', () => {
-      beforeEach(() => {
-        this.crashlytics.sendNonFatalCrash(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#setStringValueForKey', () => {
-      beforeEach(() => {
-        this.crashlytics.setStringValueForKey(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendSignUp', () => {
-      beforeEach(() => {
-        this.crashlytics.sendSignUp(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendLogin', () => {
-      beforeEach(() => {
-        this.crashlytics.sendLogin(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendCustomEvent', () => {
-      beforeEach(() => {
-        this.crashlytics.sendCustomEvent(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendStartCheckout', () => {
-      beforeEach(() => {
-        this.crashlytics.sendStartCheckout(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendContentView', () => {
-      beforeEach(() => {
-        this.crashlytics.sendContentView(null);
-      });
-
-      it('calls console.warn', () => {
-        expect(console.warn).toHaveBeenCalled();
-      });
-    });
-  });
-
-  describe('when a handler is set', () => {
-    beforeEach(() => {
-      this.handler = {
-        Crashlytics: {
-          setUserIdentifier: (id) => {
-          },
-          setUserName: (name) => {
-          },
-          setUserEmail: (email) => {
-          },
-          addLog: (log) => {
-          },
-          sendNonFatalCrash: (msg, stacktrace) => {
-          },
-          setStringValueForKey: (key, value) => {
-          },
-
-        },
-        Answers: {
-          sendSignUp: (type, success, attributes) => {
-          },
-          sendLogIn: (type, success, attributes) => {
-          },
-          sendCustomEvent: (name, attr) => {
-          },
-          sendStartCheckout: (totalPrice, currency, itemCount, attributes) => {
-          },
-          sendContentView: (name, type, id, attributes) => {
-          }
-        }
-      };
-      this.crashlytics['handler'] = this.handler;
+      this.fabricInstance = {foo: 'bar'};
+      (window as any).fabric = this.fabricInstance;
+      this.crashlytics = new Crashlytics(new PlatformMock() as Platform);
     });
 
     afterEach(() => {
-      this.handler = null;
+      (window as any).fabric = undefined;
     });
 
-    describe('#setUserIdentifier', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Crashlytics, 'setUserIdentifier').and.stub();
-        this.crashlytics.setUserIdentifier(null);
-      });
+    it('set the handler to the window.fabric instance', (done) => {
+      setTimeout(
+        () => { // wait for Platform.ready to fire
+          expect(this.crashlytics["handler"]).toEqual(this.fabricInstance);
+          done();
+        },
+        50);
+    });
+  });
 
-      it('calls this.handler.Crashlytics.setUserIdentifier', () => {
-        expect(this.handler.Crashlytics.setUserIdentifier).toHaveBeenCalled();
-      });
+  describe('without any window.fabric instance', () => {
+    beforeAll(() => {
+      this.crashlytics = new Crashlytics(new PlatformMock() as Platform);
     });
 
-    describe('#setUserName', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Crashlytics, 'setUserName').and.stub();
-        this.crashlytics.setUserName(null);
-      });
-
-      it('calls this.handler.Crashlytics.setUserName', () => {
-        expect(this.handler.Crashlytics.setUserName).toHaveBeenCalled();
-      });
+    afterAll(() => {
+      this.crashlytics = null;
     });
 
-    describe('#setUserEmail', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Crashlytics, 'setUserEmail').and.stub();
-        this.crashlytics.setUserEmail(null);
-      });
-
-      it('calls this.handler.Crashlytics.setUserEmail', () => {
-        expect(this.handler.Crashlytics.setUserEmail).toHaveBeenCalled();
-      });
-    });
-
-    describe('#addLog', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Crashlytics, 'addLog').and.stub();
-        this.crashlytics.addLog(null);
-      });
-
-      it('calls this.handler.Crashlytics.addLog', () => {
-        expect(this.handler.Crashlytics.addLog).toHaveBeenCalled();
+    describe('#sendNonFatalCrashWithStacktraceCreation', () => {
+      it('calls sendNonFatalCrash', (done) => {
+        spyOn(this.crashlytics, 'sendNonFatalCrash').and.stub();
+        spyOn(stacktrace, 'get').and.returnValue(new Promise((resolve, reject) => {
+          resolve(null);
+        }));
+        this.crashlytics.sendNonFatalCrashWithStacktraceCreation();
+        setTimeout(
+          () => {
+            expect(this.crashlytics.sendNonFatalCrash).toHaveBeenCalled();
+            done();
+          },
+          100);
       });
     });
 
-    describe('#sendNonFatalCrash', () => {
+    describe('when no handler is set', () => {
       beforeEach(() => {
-        spyOn(this.handler.Crashlytics, 'sendNonFatalCrash').and.stub();
-        this.crashlytics.sendNonFatalCrash(null);
+        this.crashlytics['handler'] = undefined;
+        spyOn(console, 'warn').and.stub();
       });
 
-      it('calls this.handler.Crashlytics.sendNonFatalCrash', () => {
-        expect(this.handler.Crashlytics.sendNonFatalCrash).toHaveBeenCalled();
+      describe('#setUserIdentifier', () => {
+        beforeEach(() => {
+          this.crashlytics.setUserIdentifier(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#setUserName', () => {
+        beforeEach(() => {
+          this.crashlytics.setUserName(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#setUserEmail', () => {
+        beforeEach(() => {
+          this.crashlytics.setUserEmail(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#addLog', () => {
+        beforeEach(() => {
+          this.crashlytics.addLog(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendNonFatalCrash', () => {
+        beforeEach(() => {
+          this.crashlytics.sendNonFatalCrash(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#setStringValueForKey', () => {
+        beforeEach(() => {
+          this.crashlytics.setStringValueForKey(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendSignUp', () => {
+        beforeEach(() => {
+          this.crashlytics.sendSignUp(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendLogin', () => {
+        beforeEach(() => {
+          this.crashlytics.sendLogin(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendCustomEvent', () => {
+        beforeEach(() => {
+          this.crashlytics.sendCustomEvent(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendStartCheckout', () => {
+        beforeEach(() => {
+          this.crashlytics.sendStartCheckout(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendContentView', () => {
+        beforeEach(() => {
+          this.crashlytics.sendContentView(null);
+        });
+
+        it('calls console.warn', () => {
+          expect(console.warn).toHaveBeenCalled();
+        });
       });
     });
 
-    describe('#setStringValueForKey', () => {
+    describe('when a handler is set', () => {
       beforeEach(() => {
-        spyOn(this.handler.Crashlytics, 'setStringValueForKey').and.stub();
-        this.crashlytics.setStringValueForKey(null);
+        this.handler = {
+          Crashlytics: {
+            setUserIdentifier: (id) => {
+            },
+            setUserName: (name) => {
+            },
+            setUserEmail: (email) => {
+            },
+            addLog: (log) => {
+            },
+            sendNonFatalCrash: (msg, stacktrace) => {
+            },
+            setStringValueForKey: (key, value) => {
+            },
+
+          },
+          Answers: {
+            sendSignUp: (type, success, attributes) => {
+            },
+            sendLogIn: (type, success, attributes) => {
+            },
+            sendCustomEvent: (name, attr) => {
+            },
+            sendStartCheckout: (totalPrice, currency, itemCount, attributes) => {
+            },
+            sendContentView: (name, type, id, attributes) => {
+            }
+          }
+        };
+        this.crashlytics['handler'] = this.handler;
       });
 
-      it('calls this.handler.Crashlytics.setStringValueForKey', () => {
-        expect(this.handler.Crashlytics.setStringValueForKey).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendSignUp', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Answers, 'sendSignUp').and.stub();
-        this.crashlytics.sendSignUp(null);
+      afterEach(() => {
+        this.handler = null;
       });
 
-      it('calls this.handler.Answers.sendSignUp', () => {
-        expect(this.handler.Answers.sendSignUp).toHaveBeenCalled();
-      });
-    });
+      describe('#setUserIdentifier', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Crashlytics, 'setUserIdentifier').and.stub();
+          this.crashlytics.setUserIdentifier(null);
+        });
 
-    describe('#sendLogIn', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Answers, 'sendLogIn').and.stub();
-        this.crashlytics.sendLogin(null);
-      });
-
-      it('calls this.handler.Answers.sendLogIn', () => {
-        expect(this.handler.Answers.sendLogIn).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendCustomEvent', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Answers, 'sendCustomEvent').and.stub();
-        this.crashlytics.sendCustomEvent(null);
+        it('calls this.handler.Crashlytics.setUserIdentifier', () => {
+          expect(this.handler.Crashlytics.setUserIdentifier).toHaveBeenCalled();
+        });
       });
 
-      it('calls this.handler.Answers.sendCustomEvent', () => {
-        expect(this.handler.Answers.sendCustomEvent).toHaveBeenCalled();
-      });
-    });
+      describe('#setUserName', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Crashlytics, 'setUserName').and.stub();
+          this.crashlytics.setUserName(null);
+        });
 
-    describe('#sendStartCheckout', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Answers, 'sendStartCheckout').and.stub();
-        this.crashlytics.sendStartCheckout(null);
-      });
-
-      it('calls this.handler.Answers.sendStartCheckout', () => {
-        expect(this.handler.Answers.sendStartCheckout).toHaveBeenCalled();
-      });
-    });
-
-    describe('#sendContentView', () => {
-      beforeEach(() => {
-        spyOn(this.handler.Answers, 'sendContentView').and.stub();
-        this.crashlytics.sendContentView(null);
+        it('calls this.handler.Crashlytics.setUserName', () => {
+          expect(this.handler.Crashlytics.setUserName).toHaveBeenCalled();
+        });
       });
 
-      it('calls this.handler.Answers.sendContentView', () => {
-        expect(this.handler.Answers.sendContentView).toHaveBeenCalled();
+      describe('#setUserEmail', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Crashlytics, 'setUserEmail').and.stub();
+          this.crashlytics.setUserEmail(null);
+        });
+
+        it('calls this.handler.Crashlytics.setUserEmail', () => {
+          expect(this.handler.Crashlytics.setUserEmail).toHaveBeenCalled();
+        });
+      });
+
+      describe('#addLog', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Crashlytics, 'addLog').and.stub();
+          this.crashlytics.addLog(null);
+        });
+
+        it('calls this.handler.Crashlytics.addLog', () => {
+          expect(this.handler.Crashlytics.addLog).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendNonFatalCrash', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Crashlytics, 'sendNonFatalCrash').and.stub();
+          this.crashlytics.sendNonFatalCrash(null);
+        });
+
+        it('calls this.handler.Crashlytics.sendNonFatalCrash', () => {
+          expect(this.handler.Crashlytics.sendNonFatalCrash).toHaveBeenCalled();
+        });
+      });
+
+      describe('#setStringValueForKey', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Crashlytics, 'setStringValueForKey').and.stub();
+          this.crashlytics.setStringValueForKey(null);
+        });
+
+        it('calls this.handler.Crashlytics.setStringValueForKey', () => {
+          expect(this.handler.Crashlytics.setStringValueForKey).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendSignUp', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Answers, 'sendSignUp').and.stub();
+          this.crashlytics.sendSignUp(null);
+        });
+
+        it('calls this.handler.Answers.sendSignUp', () => {
+          expect(this.handler.Answers.sendSignUp).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendLogIn', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Answers, 'sendLogIn').and.stub();
+          this.crashlytics.sendLogin(null);
+        });
+
+        it('calls this.handler.Answers.sendLogIn', () => {
+          expect(this.handler.Answers.sendLogIn).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendCustomEvent', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Answers, 'sendCustomEvent').and.stub();
+          this.crashlytics.sendCustomEvent(null);
+        });
+
+        it('calls this.handler.Answers.sendCustomEvent', () => {
+          expect(this.handler.Answers.sendCustomEvent).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendStartCheckout', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Answers, 'sendStartCheckout').and.stub();
+          this.crashlytics.sendStartCheckout(null);
+        });
+
+        it('calls this.handler.Answers.sendStartCheckout', () => {
+          expect(this.handler.Answers.sendStartCheckout).toHaveBeenCalled();
+        });
+      });
+
+      describe('#sendContentView', () => {
+        beforeEach(() => {
+          spyOn(this.handler.Answers, 'sendContentView').and.stub();
+          this.crashlytics.sendContentView(null);
+        });
+
+        it('calls this.handler.Answers.sendContentView', () => {
+          expect(this.handler.Answers.sendContentView).toHaveBeenCalled();
+        });
       });
     });
   });
