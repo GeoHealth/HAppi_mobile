2ffe34ef3a9cc53fef09cc808d584773ad876445
Kagelmacher <alexandre.hauet@gmail.com>
2017-05-24 17:11:04 +0200

    fixe issues

--- e2e/page-objects/login.page-objects.ts
+++ e2e/page-objects/login.page-objects.ts
@@ -1,4 +1,4 @@
-import { browser, element, by, ElementFinder } from 'protractor';
+import { browser } from 'protractor';
 import { PageObject } from "./page-object";
 
 export class LoginPageObject extends PageObject {
