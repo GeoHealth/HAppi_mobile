efe29c87a7d8045821f33f39814fc98b2646dfdc
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-05-24 12:26:15 +0200

    ref #157 register page starting e2e tests

---
+++ e2e/page-objects/register.page-objects.ts
@@ -0,0 +1,46 @@
+import { browser, by, element, ElementFinder } from 'protractor';
+import { PageObject } from "./page-object";
+import { LoginPageObject } from "./login.page-objects";
+
+export class RegisterPageObject extends PageObject {
+  loginPage: LoginPageObject = new LoginPageObject();
+
+  browseToPage(): void{
+    browser.get('');
+    this.loginPage.getRegisterButton().click();
+    browser.driver.sleep(500);
+  }
+
+
+  getFirstNameValue() {
+    return element(by.name('first_name')).getText();
+  }
+
+  getLastNameValue() {
+    return element(by.name('last_name')).getText();
+  }
+
+  getGenderNameValue() {
+    return element(by.name('gender')).getText();
+  }
+
+  getEmailValue() {
+    return element(by.name('email')).getText();
+  }
+
+  getPasswordValue() {
+    return element(by.name('password')).getText();
+  }
+
+  getPasswordConfirmationValue() {
+    return element(by.name('password_confirmation')).getText();
+  }
+
+  isAgreementWithTermChecked() {
+    return element(by.name('condition')).getText();
+  }
+
+  getRegisterButton(): ElementFinder {
+    return element(by.css(".submit-btn"));
+  }
+}
---
+++ e2e/register.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { RegisterPageObject } from "./page-objects/register.page-objects";
+
+let registerPageObject: RegisterPageObject = new RegisterPageObject();
+
+describe('Registration page E2E Test', () => {
+  beforeEach(() => {
+    registerPageObject.browseToPage();
+  });
+
+  it('starts with an empty form', () => {
+    expect(registerPageObject.getFirstNameValue()).toEqual('');
+    expect(registerPageObject.getLastNameValue()).toEqual('');
+    expect(registerPageObject.getGenderNameValue()).toEqual('');
+    expect(registerPageObject.getEmailValue()).toEqual('');
+    expect(registerPageObject.getPasswordValue()).toEqual('');
+    expect(registerPageObject.getPasswordConfirmationValue()).toEqual('');
+    expect(registerPageObject.isAgreementWithTermChecked()).toBeFalsy();
+  });
+
+  it('disables the "register" button when the form is not valid', () => {
+    expect(registerPageObject.getRegisterButton().isEnabled()).toBeFalsy();
+  });
+
+});
