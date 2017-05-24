68864003f227c0c6d36f3217d34e9198e24a25f8
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-05-24 17:59:59 +0200

    ref #157 registration basic tests

--- e2e/register.e2e-spec.ts
+++ e2e/register.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { RegisterPageObject } from "./page-objects/register.page-objects";
+import { beforeEach } from "selenium-webdriver/testing";
 
 let registerPageObject: RegisterPageObject = new RegisterPageObject();
 
@@ -7,18 +8,19 @@ describe('Registration page E2E Test', () => {
     registerPageObject.browseToPage();
   });
 
-  it('starts with an empty form', () => {
-    expect(registerPageObject.getFirstNameValue()).toEqual('');
-    expect(registerPageObject.getLastNameValue()).toEqual('');
-    expect(registerPageObject.getGenderNameValue()).toEqual('');
-    expect(registerPageObject.getEmailValue()).toEqual('');
-    expect(registerPageObject.getPasswordValue()).toEqual('');
-    expect(registerPageObject.getPasswordConfirmationValue()).toEqual('');
-    expect(registerPageObject.isAgreementWithTermChecked()).toBeFalsy();
-  });
+  describe('on page load', () => {
+    it('starts with an empty form', () => {
+      expect(registerPageObject.getFirstNameValue()).toEqual('');
+      expect(registerPageObject.getLastNameValue()).toEqual('');
+      expect(registerPageObject.getGenderNameValue()).toEqual('');
+      expect(registerPageObject.getEmailValue()).toEqual('');
+      expect(registerPageObject.getPasswordValue()).toEqual('');
+      expect(registerPageObject.getPasswordConfirmationValue()).toEqual('');
+      expect(registerPageObject.isAgreementWithTermChecked()).toBeFalsy();
+    });
 
-  it('disables the "register" button when the form is not valid', () => {
-    expect(registerPageObject.getRegisterButton().isEnabled()).toBeFalsy();
+    it('disables the "register" button when the form is not valid', () => {
+      expect(registerPageObject.getRegisterButton().isEnabled()).toBeFalsy();
+    });
   });
-
 });
